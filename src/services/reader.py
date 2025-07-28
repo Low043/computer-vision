98df36f518ca5f7e1060989b710e16681ac467f1
Elton <elton.felype.s.nascimento@gmail.com>
2025-07-28 08:46:56 -0300

    Usando threshold adaptativo no processamento da imagem.

--- src/services/reader.py
+++ src/services/reader.py
@@ -17,11 +17,13 @@ class Reader(easyocr.Reader):
     def get_text_from_frame(self, frame: np.ndarray, save_frame: bool = False):
         """Pré-processa a imagem, e extrai o texto usando o easyOCR"""
         try:
-            cropped_img = frame[100:500, 100:500]
+            cropped_img = frame[220:360, 280:580]
             blur_img = cv2.GaussianBlur(cropped_img, (5, 5), 0)
             gray_img = cv2.cvtColor(blur_img, cv2.COLOR_BGR2GRAY)
+            threshold_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 19, 3)
 
-            final_img = gray_img
+
+            final_img = threshold_img
 
             if save_frame:
                 cv2.imwrite(f'{self.save_folder}/processed.png', final_img)
@@ -29,7 +31,13 @@ class Reader(easyocr.Reader):
             result = self.readtext(final_img, allowlist='0123456789')
             text = ''.join([res[1] for res in result])
 
-            return text
+            return text  
 
         except Exception as e:
             return f'Erro ao extrair texto: {e}'
+
+    def to_threshold(self, img: np.ndarray):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        fimg = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 1)
+        cv2.imwrite('src/images/thresholded.png', fimg)
+
