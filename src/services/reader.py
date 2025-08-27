4b54ee041b92712e4a95daf63c9265dcef84339a
unknown <luisgustavopereiraf@gmail.com>
2025-08-27 08:26:53 -0300

    feat: ignorando overlaps e reader refatorado

--- src/services/reader.py
+++ src/services/reader.py
@@ -1,5 +1,12 @@
 """ Declaração da classe Reader"""
 from ultralytics import YOLO
+from typing import TypedDict
+
+class ExtractedValue(TypedDict):
+    text: str
+    x1: float
+    x2: float
+    conf: float
 
 class Reader:
     """Classe responsável por ler os frames da stream e processar o resultado"""
@@ -12,11 +19,40 @@ class Reader:
 
         extracted_values = []
         for box in result.boxes:
-            text_value = result.names.get(box.cls.item())
-            x1 = box.xyxy[0][0].item()
-            extracted_values.append([text_value, x1])
+            extracted_values.append(self.__get_box_values(box, result.names))
 
-        ordered_values = sorted(extracted_values, key=lambda x: x[1])
-        final_result = ''.join([value[0] for value in ordered_values]).replace('.','')
+        final_result = self.__refine_values(extracted_values)
 
         return [final_result, result.plot()]
+    
+    def __get_box_values(self, box, names) -> ExtractedValue:
+        return {
+            'text': names.get(box.cls.item()),
+            'x1': box.xyxy[0][0].item(),
+            'x2': box.xyxy[0][2].item(),
+            'conf': box.conf.item()
+        }
+    
+    def __refine_values(self, values: list[ExtractedValue]):
+        ordered_values = self.__order_values(values)
+        no_overlap_values = self.__remove_overlaps(ordered_values)
+        final_result = self.__join_values(no_overlap_values)
+
+        return final_result
+    
+    def __order_values(self, values: list[ExtractedValue]):
+        return sorted(values, key=lambda x: x['x1'])
+    
+    def __remove_overlaps(self, ordered_values: list[ExtractedValue]):
+        for i in range(len(ordered_values) - 1):
+            current = ordered_values[i]
+            next = ordered_values[i + 1]
+
+            if next['x1'] < current['x2']:
+                minor_conf = current if current['conf'] < next['conf'] else next
+                minor_conf['text'] = ''
+
+        return ordered_values
+    
+    def __join_values(self, values: list[ExtractedValue]):
+        return ''.join([value['text'] for value in values]).replace('.','')
